5ebfceaa09cce26072c42c1c21869198c027a853
divafizha <153896411+divafizha@users.noreply.github.com>
2025-07-04 23:53:58 +0700

    Update MainApplication.kt

--- app/src/main/java/com/cekroda/MainApplication.kt
+++ app/src/main/java/com/cekroda/MainApplication.kt
@@ -4,6 +4,9 @@ import android.app.Application
 import com.cekroda.di.appModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.GlobalContext.startKoin
+import com.cekroda.utils.AnalyticsHelper
+import com.google.firebase.analytics.FirebaseAnalytics
+import org.koin.android.ext.android.get
 
 class MainApplication: Application() {
     override fun onCreate() {
@@ -13,5 +16,8 @@ class MainApplication: Application() {
             androidContext(this@MainApplication)
             modules(appModule)
         }
+        val firebaseAnalytics = get<FirebaseAnalytics>()
+        AnalyticsHelper.init(firebaseAnalytics)
+
     }
 }
